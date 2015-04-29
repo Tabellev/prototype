ee3bd58bb6e1d4125ef9af14ee44aee994db84a0
Øyvind Trinborg <oeyvind.trinborg@gmail.com>
2015-04-29 09:02:12 +0200

    Created skeleton layout file and class for end screen.

---
+++ app/src/main/java/com/example/isabel/prototypestart/EndScreenFragment.java
@@ -0,0 +1,10 @@
+package com.example.isabel.prototypestart;
+
+import android.support.v4.app.Fragment;
+
+/**
+ * Created by oyvind on 28.04.2015.
+ */
+public class EndScreenFragment extends Fragment {
+
+}
